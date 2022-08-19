524cd54edc11b7f8668ee413dee82400c8e242ce
NassimSdn <107467782+NassimSdn@users.noreply.github.com>
2022-08-19 14:31:42 +0200

    Js panier

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,14 +1,38 @@
-// Ajout d'un article dans le panier => référence de l'article
-// Suppression d'un article du panier l'article => référence de l'article
-// Vider le panier
-// incremente la quatité => référence de l'article
-// décrémente la quantité => référence de l'article
-// Calcul Total Panier
-// Calcul Total article => reference de l'article
-// Charger le JSON
-// Fonction que me renvoi la liste des mes articles en fonction d'une cetégorie ou non =› catégorie
-// Fonction permettant d'injecter les articles dans le DOM => tableau d'oject des articles
-// Fonction permettant de créer une card à partir d'un article => article
-// Afficher le panier
-// récupèrer les information d'un article => réference de l'article
-// Afficher toutes les quantités du panier
+var sidenav = document.getElementById("mySidenav");
+var openBtn = document.getElementById("openBtn");
+var closeBtn = document.getElementById("closeBtn");
+
+openBtn.onclick = openNav;
+closeBtn.onclick = closeNav;
+
+/* Set the width of the side navigation to 250px */
+function openNav() {
+  sidenav.classList.add("active");
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  sidenav.classList.remove("active");
+}
+
+// Fonction permettant d'ouvrir le panier.
+
+let modal = document.getElementById('myModal');
+
+let button = document.getElementById('btnBasket');
+
+let span = document.getElementsByClassName('closeModal')[0];
+
+button.onclick = function () {
+    modal.style.display = 'block';    
+}
+
+span.onclick = function () {
+    modal.style.display = 'none';
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = 'none';
+    }
+}
