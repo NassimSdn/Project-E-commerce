fa42189cf7f36d9cc86a95da4661a22c779e5683
NassimSdn <107467782+NassimSdn@users.noreply.github.com>
2022-08-19 10:36:54 +0200

    Fonction à développer

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,11 +1,14 @@
-// Fonction permettant de pouvoir ajouté un ou des articles au panier
-
-// Fonction permettant de pouvoir supprimer un ou des articles au panier
-
-// Fonction permettant de pouvoir quantifié un ou des articles au panier
-
-// Fonction permettant de pouvoir modifier la quantité d'un article au panier
-
-// Fonction permettant de pouvoir calculer le coût totale du panier
-
-// Fonction permettant de pouvoir calculer le coût d'une catégorie d'article
+// Ajout d'un article dans le panier => référence de l'article
+// Suppression d'un article du panier l'article => référence de l'article
+// Vider le panier
+// incremente la quatité => référence de l'article
+// décrémente la quantité => référence de l'article
+// Calcul Total Panier
+// Calcul Total article => reference de l'article
+// Charger le JSON
+// Fonction que me renvoi la liste des mes articles en fonction d'une cetégorie ou non =› catégorie
+// Fonction permettant d'injecter les articles dans le DOM => tableau d'oject des articles
+// Fonction permettant de créer une card à partir d'un article => article
+// Afficher le panier
+// récupèrer les information d'un article => réference de l'article
+// Afficher toutes les quantités du panier
