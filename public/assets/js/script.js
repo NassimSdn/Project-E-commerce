a49869876f700c4b3f35516c34acb2f8e5ec0b0e
Tristan <waquet_tristan@outlook.fr>
2022-08-20 17:37:04 +0200

    Fin du style mobile.

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -26,18 +26,78 @@ let button = document.getElementById('btnBasket');
 let span = document.getElementsByClassName('closeModal')[0];
 
 button.onclick = function () {
-    modal.style.display = 'block';    
+  modal.style.display = 'block';
 }
 
 span.onclick = function () {
-    modal.style.display = 'none';
+  modal.style.display = 'none';
 }
 
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = 'none';
-    }
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = 'none';
+  }
 }
+
+// Fonction créant les cards des Polos.
+
+let cardPolo = document.getElementById('cardPolo');
+
+fetch('./public/assets/js/product.json')
+  .then(response => response.json())
+  .then(function (data) {
+    data.results.forEach(product => {
+      cardPolo.innerHTML += 
+        `<div class="cardArticle">
+            <img class="pictureCard" src="${product.poster_path}"/>
+            <div class="detailsCard">
+            <h4 class="titleCard">${product.name}</h4>
+            <p class="priceCard">Prix : ${product.price}€</p>
+            <p class="sizeCard">Taille : ${product.Size}</p>
+            </div>
+        </div>`
+    })
+  })
+
+  // Fonction créant les cards des Shorts.
+
+  let cardShort = document.getElementById('cardShort');
+
+  fetch('./public/assets/js/product.json')
+    .then(response => response.json())
+    .then(function (data) {
+      data.results.forEach(product => {
+        cardShort.innerHTML += 
+          `<div class="cardArticle">
+              <img class="pictureCard" src="${product.poster_path}"/>
+              <div class="detailsCard">
+              <h4 class="titleCard">${product.name}</h4>
+              <p class="priceCard">Prix : ${product.price}€</p>
+              <p class="sizeCard">Taille : ${product.Size}</p>
+              </div>
+          </div>`
+      })
+    })
+
+  // Fonction créant les cards des Espadrilles.
+
+  let cardShoes = document.getElementById('cardShoes');
+
+fetch('./public/assets/js/product.json')
+  .then(response => response.json())
+  .then(function (data) {
+    data.results.forEach(product => {
+      cardShoes.innerHTML += 
+        `<div class="cardArticle">
+            <img class="pictureCard" src="${product.poster_path}"/>
+            <div class="detailsCard">
+            <h4 class="titleCard">${product.name}</h4>
+            <p class="priceCard">Prix : ${product.price}€</p>
+            <p class="sizeCard">Taille : ${product.Size}</p>
+            </div>
+        </div>`
+    })
+  })
 
 // Fonction permettant de pouvoir ajouté un ou des articles au panier
 
