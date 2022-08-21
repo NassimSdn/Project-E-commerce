438a52d91dc606458f9d4be366e012f7402971c0
Tristan <waquet_tristan@outlook.fr>
2022-08-21 14:06:12 +0200

    Fin du desktop.

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,5 +1,7 @@
 //Fonction de navbar.
 
+// Fonction ouvrant le menu de category.
+
 var sidenav = document.getElementById("mySidenav");
 var openBtn = document.getElementById("openBtn");
 var closeBtn = document.getElementById("closeBtn");
@@ -7,12 +9,10 @@ var closeBtn = document.getElementById("closeBtn");
 openBtn.onclick = openNav;
 closeBtn.onclick = closeNav;
 
-/* Set the width of the side navigation to 250px */
 function openNav() {
   sidenav.classList.add("active");
 }
 
-/* Set the width of the side navigation to 0 */
 function closeNav() {
   sidenav.classList.remove("active");
 }
